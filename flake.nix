37aa7902664e54d0ec3dae0c7d19810e6591a951
Yaroslav Bolyukin <iam@lach.pw>
2023-06-06 21:18:02 +0200

    ci: add nix flake

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Auth service";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [rust-overlay.overlay];
+          config.allowUnfree = true;
+        };
+        rust =
+          (pkgs.rustChannelOf {
+            date = "2023-05-09";
+            channel = "nightly";
+          })
+          .default
+          .override {
+            extensions = ["rust-analyzer" "rust-src", "clippy"];
+          };
+      in rec {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            rust
+            cargo-edit
+            cargo-expand
+            cargo-watch
+          ];
+        };
+      }
+    );
+}
