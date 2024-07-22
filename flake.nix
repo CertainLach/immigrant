a726158091b5f8561cf5271db2a997988ab21b8f
Yaroslav Bolyukin <iam@lach.pw>
2024-07-22 22:26:36 +0200

    build: upgrade dependencies

--- flake.nix
+++ flake.nix
@@ -2,33 +2,43 @@
   description = "Immigrant: Database schema description language";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     crane = {
       url = "github:ipetkov/crane";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    shelly = {
+      url = "github:CertainLach/shelly";
+      inputs = {
+        flake-parts.follows = "flake-parts";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
   };
-  outputs = {
+  outputs = inputs @ {
     nixpkgs,
-    flake-utils,
+    flake-parts,
+    shelly,
     rust-overlay,
     crane,
     ...
   }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [rust-overlay.overlays.default];
-          config.allowUnfree = true;
-        };
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [shelly.flakeModule];
+      perSystem = {
+        config,
+        system,
+        pkgs,
+        ...
+      }: let
         rust =
           pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
         craneLib = (crane.mkLib pkgs).overrideToolchain rust;
@@ -38,6 +48,11 @@
           cmake
         ];
       in rec {
+        _module.args.pkgs = import nixpkgs {
+          inherit system;
+          overlays = [rust-overlay.overlays.default];
+          config.allowUnfree = true;
+        };
         packages = {
           default = packages.immigrant;
           immigrant = craneLib.buildPackage {
@@ -47,18 +62,22 @@
             nativeBuildInputs = sharedDeps;
           };
         };
-        devShell = craneLib.devShell {
-          packages = with pkgs; [
-            alejandra
-            cargo-edit
+        shelly.shells.default = {
+          factory = craneLib.devShell;
+          packages = with pkgs;
+            [
+              cargo-edit
+              just
 
-            just
-
-            nodePackages_latest.yarn
-            nodejs_latest
-            tree-sitter
-          ] ++ sharedDeps;
+              tree-sitter
+              (yarn-berry.override {nodejs = nodejs_latest;})
+              nodejs_latest
+              # Wanted by node-nan, which is used by tree-sitter, even if js binding is not actually used.
+              python3
+            ]
+            ++ sharedDeps;
         };
-      }
-    );
+        formatter = pkgs.alejandra;
+      };
+    };
 }
